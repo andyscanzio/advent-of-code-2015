aa9668054735a0ffeb122d0357a207c5278a3288
Andrea Scanzio <andyscanzio@gmail.com>
2022-12-11 14:15:01 +0100

    Day1

--- day01.py
+++ day01.py
@@ -1,18 +1,21 @@
 import utils
 
-elves = utils.load_input("day01")
+floors = utils.load_input("day01")
 
 
 def part1(text: str) -> int:
-    elves = [sum(map(int, elf.rstrip("\n").split("\n"))) for elf in text.split("\n\n")]
-    return max(elves)
+    return sum(1 if floor == "(" else -1 for floor in text)
 
 
 def part2(text: str) -> int:
-    elves = [sum(map(int, elf.rstrip("\n").split("\n"))) for elf in text.split("\n\n")]
-    return sum(sorted(elves, reverse=True)[:3])
+    floor = 0
+    for idx, char in enumerate(text):
+        floor += 1 if char == "(" else -1
+        if floor < 0:
+            return idx + 1
+    return -1
 
 
 if __name__ == "__main__":
-    print(part1(elves))
-    print(part2(elves))
+    print(part1(floors))
+    print(part2(floors))
--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import day01
 
 class TestDay01(unittest.TestCase):
     def setUp(self):
-        self.text_list = [
+        self.text_part1_list = [
             "(())",
             "()()",
             "(((",
@@ -16,8 +16,14 @@ class TestDay01(unittest.TestCase):
             ")))",
             ")())())",
         ]
-        self.result_list = [0, 0, 3, 3, 3, -1, -1, -3, -3]
+        self.result_part1_list = [0, 0, 3, 3, 3, -1, -1, -3, -3]
+        self.text_part2_list = [")", "()())"]
+        self.result_part2_list = [1, 5]
 
     def test_part1(self):
-        for text, result in zip(self.text_list, self.result_list):
+        for text, result in zip(self.text_part1_list, self.result_part1_list):
             self.assertEqual(day01.part1(text), result)
+
+    def test_part2(self):
+        for text, result in zip(self.text_part2_list, self.result_part2_list):
+            self.assertEqual(day01.part2(text), result)
